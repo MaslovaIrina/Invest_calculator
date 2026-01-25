cd3efe2c0a49ad1484f25e5ee481343a4ec64d77
MaslovaIrina <kenar_00@mail.ru>
2026-01-25 16:07:00 +0300

    Add new form inputs and show extra fields when mortgage is enabled

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
+from typing import Optional
+from pydantic import BaseModel, Field, model_validator
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Real Estate Calculator API")
@@ -14,10 +15,32 @@ app.add_middleware(
 
 
 class CalcRequest(BaseModel):
-    purchase_price: float = Field(gt=0, description="Цена объекта")
-    monthly_rent: float = Field(ge=0, description="Аренда в месяц")
-    monthly_expenses: float = Field(ge=0, description="Расходы в месяц")
-    monthly_salary: float = Field(ge=0, description='Зарплата')
+    purchase_price: float = Field(gt=0, description="Цена новой недвижимости")
+    monthly_rent: float = Field(ge=0, description="Стоимость аренды в месяц")
+    yearly_rent_increase: float = Field(ge=0, description="Удорожание стоимости аренды за год")
+    monthly_free_money: float = Field(ge=0, description='Свободных денег для вложений в месяц')
+    all_free_money: float = Field(ge=0, description='Свободных денег для вложений уже есть')
+    invest_percent: float = Field(ge=0, description="Процентов годовых на вкладе")
+    yearly_apart_price_change: float = Field(description="Изменение цены новой недвижимости в год")
+    monthly_unexpected_expenses: float = Field(ge=0, description="Непредвиденные расходы в месяц")
+    #yearly_new_building_price_change: float = Field(ge=0, description="Рост цены в новостройке засчет приближения к сроку сдачи")
+    #is_new_building: bool = Field(default=False, description="Новостройка?")
+    has_mortgage: bool = Field(default=False, description="Есть ипотека?")
+    mortgage_term: Optional[float] = Field(default=None, gt=0, description="Срок ипотеки (лет)")
+    ipotek_percent: Optional[float] = Field(ge=0, description="Процент по ипотеке годовых")
+
+
+
+    @model_validator(mode="after")
+    def check_mortgage_fields(self):
+        if self.has_mortgage:
+            if self.all_free_money > self.purchase_price:
+                raise ValueError("all_free_money не может быть больше purchase_price")
+            if self.mortgage_term > 31:
+                raise ValueError('Ипотеку выдают не более, чем на 30 лет')
+        return self
+
+
 
 class CalcResponse(BaseModel):
     monthly_cashflow: float
@@ -26,7 +49,7 @@ class CalcResponse(BaseModel):
 
 @app.post("/api/calc", response_model=CalcResponse)
 def calc(req: CalcRequest) -> CalcResponse:
-    monthly_cashflow = req.monthly_rent - req.monthly_expenses
+    monthly_cashflow = req.monthly_rent - req.monthly_unexpected_expenses
     yearly_cashflow = monthly_cashflow * 12
 
     # “Грубая доходность” (без тонкостей NOI/вакансии/налогов) — чисто для демонстрации
